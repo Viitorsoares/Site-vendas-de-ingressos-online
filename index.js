ef008bd8fae38f91efab62920277cb0f57aef629
Viitorsoares <viitorgab.soares@gmail.com>
2024-02-20 03:10:54 -0300

    Atualizando css do projeto

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-import express from 'express'
-import process from 'process'
-import path from 'path'
+import express from 'express';
+import process from 'process';
+import path from 'path';
+import session from 'express-session';
+import autenticar from './seguranca/autenticar.js';
 
-const site = express()
+const host = '0.0.0.0';
+const porta = 3000;
 
-const host = '0.0.0.0'
-const porta = 3002
+const site = express();
 
-site.use(express.static(path.join(process.cwd(), 'publico')))
+site.use(express.urlencoded({extended: true}));
 
-site.use(express.static(path.join(process.cwd(), 'privado')))
+site.use(session({
+    secret: 'ChAvEsEcReTa',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 60 * 1000 * 15
+    }
+}))
+
+site.post('/login', (requisicao, resposta)=> {
+    const usuario = requisicao.body.usuario;
+    const senha = requisicao.body.senha;
+    if (usuario && senha && usuario === 'Vitor' && senha === '123') {
+        requisicao.session.usuarioLogado = true;
+        resposta.redirect('/index.html');
+    } else {
+        resposta.redirect('/login.html');
+    }
+})
+
+site.use(express.static(path.join(process.cwd(), 'publico')));
+
+site.use(autenticar, express.static(path.join(process.cwd(), 'privado')));
 
 site.listen(porta, host, ()=> {
-    console.log(`Servidor escutando em http://${host}:${porta}`)
+    console.log(`Servidor escutando em http://${host}:${porta}`);
 })
+
--- publico/scriptLogin.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('formLogin').addEventListener('submit', (ev)=> {
-    if (form.checkValidity() === false) {
-        form.classList.add('was-validated')
-        ev.preventDefault()
-        ev.stopPropagation()
-    } else {
-        form.classList.remove('was-validated')
-    }
-})
